7ceaa011d95105905f95811b15ceec711bf52c7f
Konstantin Honcharov <k.honcharov@gmail.com>
2021-09-20 17:51:01 +0300

    feat: add side nav menu

--- js/index.js
+++ js/index.js
@@ -1,11 +1,26 @@
 const menuBtn = document.querySelector(".menu-btn-container");
+const sideNav = document.querySelector(".sidenav");
+const mobileMenuLink = document.querySelector(".mob-menu-link");
 
-menuBtn.addEventListener("click", (e) => {
-  e.stopPropagation();
-  console.log(e.currentTarget);
-  menuBtnHandler(e.currentTarget);
+const page = {
+  sideNavState: 0,
+};
+
+menuBtn.addEventListener("click", menuBtnHandler);
+
+sideNav.addEventListener("click", (e) => {
+  menuBtnHandler(e.target);
 });
 
 function menuBtnHandler(target) {
-  target.classList.toggle("change");
+  if (target.className !== "sidenav") {
+    menuBtn.classList.toggle("change");
+    if (page.sideNavState) {
+      page.sideNavState = 0;
+      sideNav.style.width = "0";
+    } else {
+      page.sideNavState = 1;
+      sideNav.style.width = "250px";
+    }
+  }
 }
